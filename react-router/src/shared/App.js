6cd351176cbf1d6db1e7a6e972d573ab801f44d9
in-kim <ehddls960617@naver.com>
2020-09-03 12:04:21 +0900

    react router 실습

---
+++ react-router/src/shared/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home, About, Posts } from "pages";
+import Menu from "../components/Menu";
+
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <Route exact path="/" component={Home} />
+      <Switch>
+        <Route path="/about/:name" component={About} />
+        <Route path="/about" component={About} />
+      </Switch>
+      <Route path="/posts" component={Posts} />
+    </div>
+  );
+};
+
+export default App;
