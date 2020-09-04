7c51a71f0c3958d76f46052b67d2746d18fc02ca
in-kim <ehddls960617@naver.com>
2020-09-04 17:24:32 +0900

    useMemo, useCallback, useRef 실습

--- react-hook/src/components/Average.js
+++ react-hook/src/components/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback, useRef } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산중...");
@@ -10,20 +10,26 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
+  }, []); //컴포넌트가 처음 렌더링 될 때만 함수 생성
 
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+      inputEl.current.focus();
+    },
+    [number, list]
+  ); // number 혹은 list 가 바뀌었을 때만 함수 생성
 
+  const avg = useMemo(() => getAverage(list), [list]);
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
@@ -31,7 +37,7 @@ const Average = () => {
         ))}
       </ul>
       <div>
-        <b>평균 값 : </b> {getAverage(list)}
+        <b>평균 값 : </b> {avg}
       </div>
     </div>
   );
