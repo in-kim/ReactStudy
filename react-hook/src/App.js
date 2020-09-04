f6f3e046fddd0f6d6989469a11a714e8daa7bcb4
in-kim <ehddls960617@naver.com>
2020-09-04 17:24:43 +0900

    no message

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import InfoParents from "./components/infoParents";
 import Info2 from "./components/Info2";
+import Info3 from "./components/Info3";
 import Count from "./components/Count";
 import Count2 from "./components/Count2";
 import ContextSample from "./components/ContextSample";
 import Average from "./components/Average";
+import RefSample from "./components/RefSample";
+import UsePromise from "./components/UsePromiseSample";
 function App() {
   return (
     <div>
@@ -12,26 +15,61 @@ function App() {
       <section>
         <InfoParents />
       </section>
-      <h2>Count ( useState)</h2>
-      <section>
-        <Count />
-      </section>
-      <h2>Context sample</h2>
-      <section>
-        <ContextSample />
-      </section>
-      <h2>Count ( useReducer )</h2>
-      <section>
-        <Count2 />
-      </section>
+
+      <hr />
+
       <h2>Info ( useReducer )</h2>
       <section>
         <Info2 />
       </section>
 
+      <hr />
+
+      <h2>Info (커스텀 Hook)</h2>
+      <section>
+        <Info3 />
+      </section>
+
+      <hr />
+
+      <h2>Count ( useState)</h2>
+      <section>
+        <Count />
+      </section>
+
+      <hr />
+
+      <h2>Context sample</h2>
+      <section>
+        <ContextSample />
+      </section>
+
+      <hr />
+
+      <h2>Count ( useReducer )</h2>
+      <section>
+        <Count2 />
+      </section>
+
+      <hr />
+
       <h2>Average</h2>
       <section>
         <Average />
+      </section>
+
+      <hr />
+
+      <h2>RefSample</h2>
+      <section>
+        <RefSample />
+      </section>
+
+      <hr />
+
+      <h2>Promise hook</h2>
+      <section>
+        <UsePromise />
       </section>
     </div>
   );
